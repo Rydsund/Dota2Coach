00c8e6dbc315f2e44dbbd1a7c65ea9b4d6f6cac2
Viktor Rydsund <62593116+Rydsund@users.noreply.github.com>
2022-05-23 11:18:07 +0200

    Delete NN_DotaAI Directory
    Removed old temp directory. Will be replaced later today.

--- NN_DotaAI/JsonToCsv.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-
-def convert(path):
-    df = pd.read_json(path, lines=True)
-    df.to_csv(path + '.csv')
--- NN_DotaAI/main.py
+++
@@ -1,9 +0,0 @@
-import Preprocess
-import JsonToCsv
-
-def main():
-    Preprocess.process()
-
-
-if __name__ == "__main__":
-    main()
